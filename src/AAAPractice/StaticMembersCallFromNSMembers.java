1d8e99b3f8834775dd45c518a7f6cda73ab48a38
Someshwar Wagh <someshwarwagh6165@gmail.com>
2023-04-09 12:35:26 +0530

    Static Members Call From NS Members

---
+++ src/AAAPractice/StaticMembersCallFromNSMembers.java
@@ -0,0 +1,60 @@
+package AAAPractice;
+
+public class StaticMembersCallFromNSMembers {
+
+	static int a=10;
+	public static void print()
+	{
+		System.out.println("static print()");
+	}
+	
+	StaticMembersCallFromNSMembers()
+	{
+		System.out.println(a);
+		System.out.println(StaticMembersCallFromNSMembers.a);
+		System.out.println(this.a);
+		print();
+		StaticMembersCallFromNSMembers.print();
+		this.print();
+	}
+	StaticMembersCallFromNSMembers(int n)
+	{
+		a=n;
+		System.out.println(a);
+		System.out.println(StaticMembersCallFromNSMembers.a);
+		System.out.println(this.a);
+		print();
+		StaticMembersCallFromNSMembers.print();
+		this.print();
+	}
+	
+	{
+		nsMethod();
+		System.out.println(a);
+		System.out.println(StaticMembersCallFromNSMembers.a);
+		System.out.println(this.a);
+		print();
+		StaticMembersCallFromNSMembers.print();
+		this.print();
+	}
+	
+	public void nsMethod()
+	{
+		System.out.println("From NS-Method");
+		System.out.println(a);
+		System.out.println(StaticMembersCallFromNSMembers.a);
+		System.out.println(this.a);
+		print();
+		StaticMembersCallFromNSMembers.print();
+		this.print();
+		
+	}
+	
+	
+	public static void main(String[] args) {
+	
+		StaticMembersCallFromNSMembers obj=new StaticMembersCallFromNSMembers();
+		StaticMembersCallFromNSMembers obj1=new StaticMembersCallFromNSMembers(20);
+	}
+
+}
