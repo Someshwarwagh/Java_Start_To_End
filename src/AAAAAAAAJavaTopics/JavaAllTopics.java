40be6e29228b3c90b6a51c2c39bc2420bef1c4c2
Someshwar Wagh <someshwarwagh6165@gmail.com>
2023-04-15 15:04:14 +0530

    Java Topics Class

---
+++ src/AAAAAAAAJavaTopics/JavaAllTopics.java
@@ -0,0 +1,104 @@
+package AAAAAAAAJavaTopics;
+
+public class JavaAllTopics {
+//
+//	1.Programming Language 
+//		- High Level
+//		- Low Level
+//		- Middle Level
+//	2.Characteristics of java and why JAVA is platform independent
+//	3.Token 
+//		- Keywords
+//		- Identifiers
+//		- Literals
+//		- Separators
+//	4.Printing Statements
+//		-print()
+//		-println()
+//	5.+ operator
+//	6.Data types
+//	    -Primitive Data types
+//			-Integer
+//				-byte
+//				-short
+//				-int
+//				-long
+//			-Float
+//				-float
+//				-double
+//			-Character
+//				-char
+//			-Boolean
+//				-boolean
+//		-Non-primitive Data types
+//			-String, Class, Array etc
+//	7.Variables
+//		-Local
+//		-Global
+//	8.Operators
+//		-Unary
+//		-Binary
+//		-Ternary
+//	9.Type Casting
+//		-Primitive Type Casting 
+//			-Widening
+//			-Narrowing
+//		-Non-Primitive Type Casting 
+//			-Up casting
+//			-Down Casting
+//	10.Decision Making Statements
+//		-if
+//		-if else
+//		-else if ladder
+//		-nested if
+//		-switch
+//	11.Looping Statements
+//		-while
+//		-do while
+//		-for
+//	12.Control transfer Statements
+//		-break
+//		-continue
+//	13.Method
+//		-MCS
+//		-Calling Method
+//		-Called Method
+//		-Return Type
+//		-Method Overloading
+//	14.Recursive Method
+//		-Base Condition
+//	15.Array
+//	16.Static Members
+//		-Loading Process Of Static Members
+//		-Accessing and initialising from other blocks ways
+//		-Static variable
+//		-Static Method
+//		-Static Block
+//		-Static Initialiser Block(SIB)
+//	17.Non Static Members
+//		-Loading Process Of NS Members
+//		-Accessing and Initialising from other blocks ways
+//		-NS Static variable
+//		-NS Static Method
+//		-NS Static Block
+//		-Instance Initialiser Block(IIB)
+//		-This Keyword
+//		-Constructor
+//			-Types No argument and parameterised
+//			-Default Constructor
+//			-Constructor Overloading
+//	18.This Keyword
+//	19.Relationships --> Has_a Relationship and Is_a Relationship
+//	20.Object Oriented Programming(oops)
+//		-Encapsulation --> Data Hiding,Private Keyword, Getter Method, Setter Method 
+//		-Inheritance
+//		-Polymorphism
+//		-Abstraction
+//	
+//	
+//	
+//	
+	
+	
+	
+}
